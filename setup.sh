bd854f6615d0bf7c01e0f54facb38ab26d5ebc93
Ryoki Kato <rkato@B71049.local>
2019-04-12 12:02:33 +0900

    modified

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 . /dnsfw-docker/etc/conf.txt
-cp 
 sed -i -e "s/\[TSIG Key\]/$TSIGKEY/" /dnsfw-docker/etc/threatstop.rpz.conf 
 sed -i -e "s|\[TSIG Key secret\]|$TSIGSECRET|" /dnsfw-docker/etc/threatstop.rpz.conf 
 sed -i -e "s/\[ZONE\]/$ZONE/" /dnsfw-docker/etc/threatstop.rpz.conf 
